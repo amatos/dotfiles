dc9af5e85fc4bd6f333f1b132b677b43ede3cd78
Alberth Matos <alberth@matos.cc>
2023-10-20 13:00:30 -0400

    zsh updates
    Updated bootstrap to check for zsh before installing oh-my-zsh and the cobalt2 theme.

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin main;
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		--exclude "cobalt2" \
+		--exclude "init" \
+		--exclude "brew.sh" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+doIt;
+unset doIt;
+
+if [ -f /usr/bin/zsh ];
+then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+	rsync -avh --no-perms ./cobalt2/cobalt2.zsh-theme ~/.oh-my-zsh/themes/cobalt2.zsh-theme
+fi
