87d93fd5253f8c2927c87cd53c616140594b2fef
Alberth Cruz Matos <alberth@matos.cc>
2023-11-11 14:18:28 -0500

    Updated to check for oh-my-zsh, and split things up more logically

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+function ohmyzsh() {
+	echo "Checking for oh-my-zsh"
+	if [ -f /usr/bin/zsh ] && [ ! -d ~/.oh-my-zsh ];
+	then
+		echo "Oh-my-zsh not found.  Deploying."
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+		rsync -ah --no-perms ./cobalt2/cobalt2.zsh-theme ~/.oh-my-zsh/themes/cobalt2.zsh-theme
+	fi
+}
 
-git pull origin main;
+function updateRepo() {
+	echo "Pulling any updates from github."
+	cd "$(dirname "${BASH_SOURCE}")";
+	git pull --quiet origin main;
+}
 
-function doIt() {
+function dotFiles() {
+	echo "Deploy .files"
 	rsync --exclude ".git/" \
 		--exclude "nvim/" \
 		--exclude ".DS_Store" \
@@ -15,17 +28,27 @@ function doIt() {
 		--exclude "cobalt2" \
 		--exclude "init" \
 		--exclude "brew.sh" \
-		-avh --no-perms . ~;
-	rsync -avh --no-perms "nvim/" ~/.config/nvim/ ~;
-	ln -s ~/.vimrc ~/.config/nvim/init.vim
+		-ah --no-perms . ~;
+	if [ ! -d ~/.config/nvim ];
+	then
+		mkdir -p ~/.config/nvim/
+	fi
+	# Copy over the nvim directories
+	rsync -ah --no-perms "nvim/" ~/.config/nvim/
+	# If it (~/.vimrc) doesn't exist, symlink nvim's init.vim to ~/.vimrc
+	if [ ! -f ~/.vimrc ];
+	then
+		ln -s ~/.vimrc ~/.config/nvim/init.vim
+	fi
 	source ~/.bash_profile;
 }
 
-doIt;
-unset doIt;
+updateRepo;
+ohmyzsh;
+dotFiles;
 
-if [ -f /usr/bin/zsh ];
-then
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-	rsync -avh --no-perms ./cobalt2/cobalt2.zsh-theme ~/.oh-my-zsh/themes/cobalt2.zsh-theme
-fi
+unset dotFiles;
+unset ohmyzsh;
+unset updateRepo;
+
+echo "All done."
