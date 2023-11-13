6c318246ff24fff7b7f33df39cbefe7b8118a9a4
Alberth Cruz Matos <alberth@matos.cc>
2023-11-13 18:53:39 -0500

    Sanitized variables in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,34 +12,33 @@ function ohmyzsh() {
 
 function updateRepo() {
 	echo "Pulling any updates from github."
-	cd "$(dirname "${BASH_SOURCE}")";
 	git pull --quiet origin main;
 }
 
 function dotFiles() {
 	echo "Deploy .files"
-	CWD=`pwd`
-	pushd ~
-	ln -fs $CWD/aliases .aliases
-	ln -fs $CWD/bash_profile .bash_profile
-	ln -fs $CWD/bash_prompt .bash_prompt
-	ln -fs $CWD/bashrc .bashrc
-	ln -fs $CWD/editorconfig .editorconfig
-	ln -fs $CWD/exports .exports
-	ln -fs $CWD/functions .functions
-	ln -fs $CWD/gitattributes .gitattributes
-	ln -fs $CWD/gitconfig .gitconfig
-	ln -fs $CWD/gitignore .gitignore
-	ln -fs $CWD/gvimrc .gvimrc
-	ln -fs $CWD/inputrc .inputrc
-	ln -fs $CWD/path .path
-	ln -fs $CWD/screenrc .screenrc
-	ln -fs $CWD/tmux.conf .tmux.conf
-	ln -fs $CWD/vim .vim
-	ln -fs $CWD/vimrc .vimrc
-	ln -fs $CWD/wgetrc .wgetrc
-	ln -fs $CWD/zshrc .zshrc
-	popd
+	CWD=$(pwd)
+	pushd ~ || exit
+	ln -fs "$CWD"/aliases .aliases
+	ln -fs "$CWD"/bash_profile .bash_profile
+	ln -fs "$CWD"/bash_prompt .bash_prompt
+	ln -fs "$CWD"/bashrc .bashrc
+	ln -fs "$CWD"/editorconfig .editorconfig
+	ln -fs "$CWD"/exports .exports
+	ln -fs "$CWD"/functions .functions
+	ln -fs "$CWD"/gitattributes .gitattributes
+	ln -fs "$CWD"/gitconfig .gitconfig
+	ln -fs "$CWD"/gitignore .gitignore
+	ln -fs "$CWD"/gvimrc .gvimrc
+	ln -fs "$CWD"/inputrc .inputrc
+	ln -fs "$CWD"/path .path
+	ln -fs "$CWD"/screenrc .screenrc
+	ln -fs "$CWD"/tmux.conf .tmux.conf
+	ln -fs "$CWD"/vim .vim
+	ln -fs "$CWD"/vimrc .vimrc
+	ln -fs "$CWD"/wgetrc .wgetrc
+	ln -fs "$CWD"/zshrc .zshrc
+	popd || exit
 	if [ ! -d ~/.config/nvim ];
 	then
 		mkdir -p ~/.config/nvim/
@@ -51,7 +50,7 @@ function dotFiles() {
 	then
 		ln -fs ~/.vimrc ~/.config/nvim/init.vim
 	fi
-	source ~/.bash_profile;
+	source "$HOME"/.bash_profile;
 }
 
 updateRepo;
