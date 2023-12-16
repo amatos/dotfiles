a64059517ec154e5fedbae814b84d122307083e0
Alberth Cruz Matos <alberth@matos.cc>
2023-12-16 15:18:16 -0500

    Moved 1password items to conf.d/1password.  Added explicit ssh agent

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,3 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-# Source find-the-command
-source /usr/share/doc/find-the-command/ftc.fish
-
-# Source 1Password CLI
-source $HOME/.config/op/plugins.sh
