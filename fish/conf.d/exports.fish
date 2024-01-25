63bcaa07cbdcba7fe3a09d58fa04b05b9fa9335d
Alberth Cruz Matos <alberth@matos.cc>
2024-01-24 22:02:19 -0500

    removed 1Password CLI entries from exports, and moved them to aur.fish function, since op takes a _while_ to load.

--- fish/conf.d/exports.fish
+++ fish/conf.d/exports.fish
@@ -10,6 +10,3 @@ set -xg MANPAGER 'less -X'
 
 set -xg EDITOR 'vi'
 set -xg VISUAL 'kate'
-
-set -xg AUR_USERNAME $(op item get "Archlinux AUR" --fields label=user)
-set -xg AUR_PASSWORD $(op item get "Archlinux AUR" --fields label=user)
