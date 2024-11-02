d7980235db727fb13a40a70a73ac20941ae50999
Alberth Matos <alberth@matos.cc>
2024-11-01 20:33:27 -0400

    Update .config/fish/conf.d/00-init.fish

--- dot_config/fish/conf.d/00-init.fish
+++ dot_config/fish/conf.d/00-init.fish
@@ -12,19 +12,7 @@ set -Ux XDG_STATE_HOME "$HOME/.local/state"
 # Machine info.
 set -gx OS "$(uname)"
 
-if command -v hostnamectl &>/dev/null
-    set -gx HOST "$(hostnamectl --static)"
-else if command -v scutil &>/dev/null # macOS
-    set -gx HOST "$(scutil --get ComputerName)"
-end
-
-# Projects dir.
-set -gx HACK "$HOME/hackin"
-
 # Editor.
 set -gx EDITOR nvim
 set -gx VISUAL "subl -w "
 set -gx SUDO_EDITOR "$EDITOR"
-
-# Use vi key bindings.
-#set -g fish_key_bindings fish_vi_key_bindings
