44d13a06e6646ddf2d788704faf88cd3abc9258c
Alberth Matos <alberth@matos.cc>
2024-11-03 16:33:01 -0500

    Add .local/share/light-mode.d/ags.sh Add .local/share/light-mode.d/hyprland.sh

---
+++ dot_local/share/light-mode.d/executable_ags.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ags -r "useTheme('$HOME/.config/ags/themes/catppuccin_latte_split.json')"
---
+++ dot_local/share/light-mode.d/executable_hyprland.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm ~/.config/hypr/themes/active.conf
+cp ~/.config/hypr/themes/latte.conf ~/.config/hypr/themes/active.conf
