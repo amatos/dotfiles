8fb2345c1175fefc686c456f126d4be3eb7629ac
Alberth Matos <alberth@matos.cc>
2024-11-03 10:16:57 -0500

    Update .local/share/dark-mode.d/gtk.sh

--- dot_local/share/dark-mode.d/executable_gtk.sh
+++ dot_local/share/dark-mode.d/executable_gtk.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-gsettings set org.gnome.desktop.interface gtk-theme "adwaita dark"   # for GTK3 apps
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"   # for GTK3 apps
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"   # for GTK4 apps
 
+gsettings set org.gnome.meld prefer-dark-theme  true
+gsettings set com.github.wwmm.easyeffects use-dark-theme true
