70ed0de1fab3c8cdc4de1570a58a9afbb26f7d63
Alberth Matos <alberth@matos.cc>
2024-12-06 20:13:28 -0500

    Add .config/nvim/lua/plugins/conform.lua Update .config/nvim/lua/plugins/gitsigns.lua Add .config/nvim/lua/plugins/lualine.lua Add .config/nvim/lua/plugins/mason-lspconfig.lua Add .config/nvim/lua/plugins/mason.lua Add .config/nvim/lua/plugins/neo-tree.lua Add .config/nvim/lua/plugins/nvim-autopairs.lua Add .config/nvim/lua/plugins/nvim-cmp.lua Add .config/nvim/lua/plugins/nvim-colorizer.lua Update .config/nvim/lua/plugins/nvim-treesitter.lua

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,16 +1,59 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
     },
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
+
+        treesitter.setup({
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
+            autotag = {
+                enable = true,
+            },
+            ensure_installed = {
+                "json",
+                "javascript",
+                "typescript",
+                "tsx",
+                "yaml",
+                "html",
+                "css",
+                "markdown",
+                "markdown_inline",
+                "bash",
+                "lua",
+                "vim",
+                "dockerfile",
+                "gitignore",
+                "c",
+                "rust",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+            rainbow = {
+                enable = true,
+                disable = { "html" },
+                extended_mode = false,
+                max_file_lines = nil,
+            },
+            context_commentstring = {
+                enable = true,
+                enable_autocmd = false,
+            },
+        })
+    end,
 }
