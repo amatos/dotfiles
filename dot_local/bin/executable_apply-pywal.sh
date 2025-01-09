28a9cf27bec4a418056e07b105393edf3bf207fe
Alberth Matos <alberth@matos.cc>
2025-01-08 19:43:21 -0500

    Update .local/bin/apply-pywal.sh

--- dot_local/bin/executable_apply-pywal.sh
+++ dot_local/bin/executable_apply-pywal.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 IMAGE=$HOME/Pictures/background.png
+# Use the result of 'darkman get' to determine whether
+# to create a light or dark color scheme.
+# n.b.: default to dark
 if [ `darkman get` == "light" ]; then
 	LIGHT="-l"
 else
 	LIGHT=""
 fi
+# Clear wal cache
 wal -c
+# Generate a new color scheme
 wal -i "$IMAGE" $LIGHT
+# Apply the color scheme
 swaync-client --reload-css
 cat ~/.cache/wal/colors-kitty.conf > ~/.config/kitty/current-theme.conf
 pywalfox update
