7727c2b70ef0d1626e012b95b10175f14f41523e
alberth <alberth@matos.cc>
2025-01-19 23:25:28 -0500

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,16 +1,16 @@
 -- Load my custom settings
-require('alberth.settings')
+require("alberth.settings")
 
 -- Run LazyVim package manager
-require('config.lazy')
+require("config.lazy")
 
 -- Specify parameters for packages
-require('lazy').setup({{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
+require("lazy").setup({ { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" } })
 
 -- Enable Telescope extensions if they are installed
-pcall(require('telescope').load_extension, 'fzf')
-pcall(require('telescope').load_extension, 'ui-select')
-pcall(require('telescope').load_extension, 'nerdy')
+pcall(require("telescope").load_extension, "fzf")
+pcall(require("telescope").load_extension, "ui-select")
+pcall(require("telescope").load_extension, "nerdy")
 
 -- Define whether I use a nerdfont or not
 vim.g.have_nerd_font = true
@@ -23,17 +23,17 @@ vim.opt.number = true
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-	vim.opt.clipboard = 'unnamedplus'
+	vim.opt.clipboard = "unnamedplus"
 end)
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
@@ -42,16 +42,15 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 5
 
 -- Set color scheme
---vim.cmd.colorscheme('catppuccin-macchiato')
+vim.cmd.colorscheme("catppuccin-macchiato")
 vim.opt.termguicolors = true
-local pywal = require('pywal')
-pywal.setup()
+local pywal = require("pywal")
+-- pywal.setup()
 
 -- NeoVide specific items
 if vim.g.neovide then
 	vim.g.neovide_cursor_vfx_mode = "railgun"
 end
-
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
