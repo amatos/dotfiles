9674dff264b739b209e456824d9915b44c7f5fe6
alberth <alberth@matos.cc>
2025-01-25 01:36:34 -0500

    Remove .local/share/dark-mode.d/hyprland.sh

--- dot_local/share/dark-mode.d/executable_hyprland.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rm ~/.config/hypr/themes/active.conf
-rm ~/.config/hypr/themes/active.css
-cp ~/.config/hypr/themes/macchiato.conf ~/.config/hypr/themes/active.conf
-cp ~/.config/hypr/themes/macchiato.css ~/.config/hypr/themes/active.css
