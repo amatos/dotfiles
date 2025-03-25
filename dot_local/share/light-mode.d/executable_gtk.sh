f68bcf2d188b5e66c8caa05709cf6591266fe4e1
Alberth Matos <alberth@matos.cc>
2025-03-25 16:23:27 -0400

    Update .local/share/light-mode.d/gtk.sh

--- dot_local/share/light-mode.d/executable_gtk.sh
+++ dot_local/share/light-mode.d/executable_gtk.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 gnome_schema="org.gnome.desktop.interface"
 
+gsettings set "$gnome_schema" color-scheme "default"   # for GTK4 apps
+gsettings set "$gnome_schema" cursor-theme "Adwaita"
+gsettings set "$gnome_schema" font-name "Adwaita Sans 12"
 gsettings set "$gnome_schema" gtk-theme "Adwaita"   # for GTK3 apps
-gsettings set "$gnome_schema" color-scheme "prefer-light"   # for GTK4 apps
-# gsettings set "$gnome_schema" gtk-theme "catppuccin-macchiato-mauve"
-gsettings set "$gnome_schema" font-name "SF Pro Display 12"
-gsettings set "$gnome_schema" cursor-theme "Bibata Classic Ice"
-gsettings set "$gnome_schema" color-scheme prefer-light
-gsettings set org.gnome.meld prefer-dark-theme  false
 gsettings set com.github.wwmm.easyeffects use-dark-theme false
+gsettings set org.gnome.meld prefer-dark-theme  false
