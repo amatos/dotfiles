caa6455f0ffd1eea7ec81e8c6c0d92783a101c20
Alberth Matos <alberth@matos.cc>
2026-01-03 17:32:14 -0500

    Add .config/fish/conf.d/20-ls.fish

---
+++ private_dot_config/private_fish/conf.d/executable_20-ls.fish
@@ -0,0 +1,34 @@
+#!/usr/bin/env fish
+
+# function ls --wraps lsd
+#     lsd --all --icon=auto --group-directories-first $argv
+# end
+
+# function lst --wraps lsd
+#     lsd --tree --color=auto $argv
+# end
+
+# function lsl --wraps lsd
+#     lsd --all --icon=auto --group-directories-first --long \
+#         --git --header $argv
+# end
+
+# function lslt --wraps lsd
+#     lsl --tree --color=auto $argv
+# end
+
+function ls --wraps eza
+    eza --color=auto --color-scale=all --color-scale-mode=gradient --icons=auto --group-directories-first $argv
+end
+
+function la --wraps eza
+    eza --color=auto --color-scale=all --color-scale-mode=gradient --icons=auto --group-directories-first -a $argv
+end
+
+function ll --wraps eza
+    eza --color=auto --color-scale=all --color-scale-mode=gradient --icons=auto --group-directories-first -l --git -h $argv
+end
+
+function lla --wraps eza
+    eza --color=auto --color-scale=all --color-scale-mode=gradient --icons=auto --group-directories-first -a -l --git -h $argv
+end
