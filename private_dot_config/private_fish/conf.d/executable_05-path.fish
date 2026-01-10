1ba7d3f55b98c65f879d2a06e5aaa0e3a0baed77
Alberth Matos <alberth@matos.cc>
2026-01-10 18:55:18 -0500

    Update .config/fish/conf.d/05-path.fish

--- private_dot_config/private_fish/conf.d/executable_05-path.fish
+++ private_dot_config/private_fish/conf.d/executable_05-path.fish
@@ -2,4 +2,4 @@
 
 fish_add_path ~/.local/bin
 fish_add_path /opt/homebrew/bin
-fish_add_path "~/Library/Application Support/JetBrains/Toolbox/scripts"
+fish_add_path ~/Library/Application\ Support/JetBrains/Toolbox/scripts
