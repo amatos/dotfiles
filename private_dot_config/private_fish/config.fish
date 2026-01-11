966f22cce667fd35d7e238899c455a0d5a33512d
Alberth Matos <alberth@matos.cc>
2026-01-11 17:01:36 -0500

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,16 @@
+# Install fisher if not already installed
+install_fisher
+
+set -q fisher_path; or set -Ux fisher_path "$HOME/.config/fish"
+
+# Standalone env vars
+set -gx EDITOR nvim
+set -gx CARGO_INSTALL_ROOT ~/.cargo
+
+# Colorize manpages using bat
+set -q MANPAGER; or set -Ux MANPAGER 'sh -c "col -bx | bat --language=man --style=grid --color=always --decorations=always"'
+set -q MANROFFOPT; or set -Ux MANROFFOPT -c
+
 if [ -e /opt/homebrew/bin/brew ]
     /opt/homebrew/bin/brew shellenv | source
 end
@@ -8,6 +21,10 @@ if status is-interactive
     fastfetch -c $HOME/.config/fastfetch/config-compact.jsonc # Display fastfetch
     starship init fish | source # Initialize starship
     jenv init - | source # Initialize jenv
+    set -gx BAT_STYLE auto
+    set -gx VISUAL=/opt/homebrew/bin/zed -w
+else
+    set -gx BAT_STYLE plain
 end
 
 fish_config theme choose "Dracula Official"
