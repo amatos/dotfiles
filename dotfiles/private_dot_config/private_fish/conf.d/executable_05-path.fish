71fe4e8744557483299518b1396f9cc74cd04b7a
Alberth Matos <alberth@matos.cc>
2026-01-24 13:46:01 -0500

    Update .config/fish/conf.d/05-path.fish

--- dotfiles/private_dot_config/private_fish/conf.d/executable_05-path.fish
+++ dotfiles/private_dot_config/private_fish/conf.d/executable_05-path.fish
@@ -3,3 +3,4 @@
 fish_add_path ~/.local/bin
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/Library/Application\ Support/JetBrains/Toolbox/scripts
+fish_add_path /opt/homebrew/Cellar/perl/5.42.0/bin
