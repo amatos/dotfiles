f8405a758b0091dd76967be042ec7c6664726b6d
Alberth Matos <alberth@matos.cc>
2026-03-01 14:21:37 -0500

    Update fish_variables Update .local/bin/run-after-macos-update.sh Change attributes of .chezmoiscripts/onchange_customize-macos.sh Change attributes of .chezmoiscripts/onchange_sudo_local.sh

--- dotfiles/private_dot_local/bin/executable_run-after-macos-update.sh
+++ dotfiles/private_dot_local/bin/executable_run-after-macos-update.sh
@@ -7,7 +7,7 @@ touch "$TEMP_FILE"
 
 echo "Applying sudo_local pam file allowing Touch ID for sudo."
 echo "Please provide your password for sudo."
-cat /etc/pam.d/sudo_local.template | sed 's/#auth/auth/g' > $TEMP_FILE
+[ ! -e /etc/pam.d/sudo_local] && cat /etc/pam.d/sudo_local.template | sed 's/#auth/auth/g' > $TEMP_FILE
 sudo cp $TEMP_FILE /etc/pam.d/sudo_local
 
 rm $TEMP_FILE
